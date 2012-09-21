435940a18bc194fe4bd06ae3d4e788de3e48ba7b
Sebastian Schürmann <sebastian@Sebastians-MacBook-Pro.local>
2012-09-21 09:35:31 +0200

    More Services code

--- test/service-test.js
+++ test/service-test.js
@@ -1,5 +1,12 @@
 var assert = require('assert');
 var services = require('../lib/services');
 
-
-
+describe('Services', function(){
+	describe('getAll', function(){
+		it('should return a list of services', function(){
+			var res = services.getAll();
+			assert.equal(typeof res, 'object');
+			assert.equal(res.length, 1);
+		});
+	});
+});
